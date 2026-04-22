9d534a58fb8219b8afcf5a3f7f36925d2dbcdd29
josiah <price.josiah95@gmail.com>
2026-04-22 09:24:51 -0700

    statusbar: Encoding + LineEndings + Indent + Language + Model segments

---
+++ cart/sweatshop/components/statusbar/segments/Language.tsx
@@ -0,0 +1,26 @@
+const React: any = require('react');
+
+import { Text } from '../../../../runtime/primitives';
+import { COLORS } from '../../../theme';
+import { StatusSegment } from '../StatusSegment';
+import { registerSegment } from '../useStatusRegistry';
+
+export function LanguageSegment(props: any) {
+  return (
+    <StatusSegment
+      onPress={props.onOpenSettings ? () => props.onOpenSettings('providers') : undefined}
+      tooltip="Language mode"
+    >
+      <Text fontSize={10} color={COLORS.textDim}>{props.languageMode}</Text>
+    </StatusSegment>
+  );
+}
+
+registerSegment({
+  id: 'language',
+  label: 'Language',
+  defaultPosition: 'right',
+  defaultVisible: true,
+  priority: 150,
+  component: LanguageSegment,
+});
