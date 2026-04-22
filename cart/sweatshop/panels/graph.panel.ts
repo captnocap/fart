eaf3da761c78d89880cfb9076f687c9f2c920a12
josiah <price.josiah95@gmail.com>
2026-04-22 09:18:57 -0700

    panel-registry: register graph panel

---
+++ cart/sweatshop/panels/graph.panel.ts
@@ -0,0 +1,14 @@
+const React: any = require('react');
+
+import { GraphPanelSurface } from '../components/graphpanel';
+import { register } from '../panel-registry';
+
+register({
+  id: 'graph',
+  title: 'Graph',
+  defaultSlot: 'right',
+  icon: 'panel-bottom',
+  component: GraphPanelSurface,
+  userVisible: true,
+  defaultOpen: false,
+});
