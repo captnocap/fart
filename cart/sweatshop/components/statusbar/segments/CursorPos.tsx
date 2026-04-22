e2abd2560e384bec94a71a9b80763bdadceefa7d
josiah <price.josiah95@gmail.com>
2026-04-22 09:24:45 -0700

    statusbar: CursorPos + FileInfo + Problems + Clock segments

---
+++ cart/sweatshop/components/statusbar/segments/CursorPos.tsx
@@ -0,0 +1,24 @@
+const React: any = require('react');
+
+import { Text } from '../../../../runtime/primitives';
+import { COLORS } from '../../../theme';
+import { StatusSegment } from '../StatusSegment';
+import { registerSegment } from '../useStatusRegistry';
+
+export function CursorPosSegment(props: any) {
+  return (
+    <StatusSegment tooltip={`Ln ${props.cursorLine}, Col ${props.cursorColumn}`}>
+      <Text fontSize={10} color={COLORS.textDim}>Ln {props.cursorLine}</Text>
+      <Text fontSize={10} color={COLORS.textDim}>Col {props.cursorColumn}</Text>
+    </StatusSegment>
+  );
+}
+
+registerSegment({
+  id: 'cursor-pos',
+  label: 'Cursor Position',
+  defaultPosition: 'left',
+  defaultVisible: true,
+  priority: 40,
+  component: CursorPosSegment,
+});
